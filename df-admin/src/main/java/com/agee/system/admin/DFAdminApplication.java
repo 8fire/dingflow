afa185ab26dfc893d5ba1b2ea541aa3d1a025f2c
8fire <459816669@qq.com>
2022-09-02 15:18:17 +0800

    初始化仓库

---
+++ df-admin/src/main/java/com/agee/system/admin/DFAdminApplication.java
@@ -0,0 +1,18 @@
+package com.agee.system.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author agee
+ * @date 2022-09-02 10:39
+ * @Description: DingFlow程序启动
+ */
+@SpringBootApplication
+public class DFAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DFAdminApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  DingFlow启动成功   ლ(´ڡ`ლ)ﾞ ");
+    }
+}
